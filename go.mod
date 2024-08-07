bbaa8cea60b4793a7cc1883649f03b3c5dd73592
Gamer <Gamer@DESKTOP-UO07FRG>
2024-08-07 10:05:34 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module Helios_ModbusRTU_readExample
+
+go 1.22.5
+
+require (
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07 // indirect
+	golang.org/x/sys v0.23.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"log"
+
+	"github.com/tarm/serial"
+)
+
+// Function to calculate CRC-16 for Modbus RTU
+func crc16(data []byte) uint16 {
+	var crc uint16 = 0xFFFF
+	for _, b := range data {
+		crc ^= uint16(b)
+		for i := 0; i < 8; i++ {
+			if (crc & 0x0001) != 0 {
+				crc = (crc >> 1) ^ 0xA001
+			} else {
+				crc >>= 1
+			}
+		}
+	}
+	return crc
+}
+
+// Function to build and send Modbus RTU request
+func sendModbusRequest(port *serial.Port, address byte, functionCode byte, startAddress uint16, numRegisters uint16) ([]byte, error) {
+	// Build request frame
+	request := []byte{
+		address,                   // Slave address
+		functionCode,              // Function code
+		byte(startAddress >> 8),   // Start address high byte
+		byte(startAddress & 0xFF), // Start address low byte
+		byte(numRegisters >> 8),   // Number of registers high byte
+		byte(numRegisters & 0xFF), // Number of registers low byte
+	}
+
+	crc := crc16(request)
+	request = append(request, byte(crc&0xFF))
+	request = append(request, byte(crc>>8))
+
+	// Send request
+	_, err := port.Write(request)
+	if err != nil {
+		return nil, err
+	}
+
+	// Read response
+	response := make([]byte, 256)
+	n, err := port.Read(response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response[:n], nil
+}
+
+// Function to parse the Modbus RTU response
+func parseModbusResponse(response []byte, numRegisters int) ([]uint16, error) {
+	if len(response) < 3+2*numRegisters {
+		return nil, fmt.Errorf("invalid response length")
+	}
+
+	// Validate CRC
+	crc := crc16(response[:len(response)-2])
+	if crc != binary.LittleEndian.Uint16(response[len(response)-2:]) {
+		return nil, fmt.Errorf("CRC check failed")
+	}
+
+	// Extract data
+	data := response[3:] // Skip address and function code
+	result := make([]uint16, numRegisters)
+	for i := 0; i < numRegisters; i++ {
+		result[i] = binary.BigEndian.Uint16(data[i*2 : (i+1)*2])
+	}
+
+	return result, nil
+}
+
+func main() {
+	// Open serial port
+	config := &serial.Config{Name: "/dev/ttyUSB0", Baud: 9600}
+	port, err := serial.OpenPort(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer port.Close()
+
+	// Read FAN_SPEED from address 4353
+	fanSpeedResponse, err := sendModbusRequest(port, 1, 0x03, 4353, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fanSpeed, err := parseModbusResponse(fanSpeedResponse, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("FAN_SPEED: %d\n", fanSpeed[0])
+
+	// Read Multisensor_temp from address 4363 (12-bit value)
+	tempResponse, err := sendModbusRequest(port, 1, 0x03, 4363, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tempData, err := parseModbusResponse(tempResponse, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tempValue := tempData[0] & 0x0FFF // Mask to 12 bits
+	fmt.Printf("Multisensor_temp: %d\n", tempValue)
+
+	// Read state from address 4609 (0 or 1)
+	stateResponse, err := sendModbusRequest(port, 1, 0x03, 4609, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stateData, err := parseModbusResponse(stateResponse, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stateValue := stateData[0] & 0x01 // Mask to 1 bit
+	state := "home"
+	if stateValue == 1 {
+		state = "away"
+	}
+
+	fmt.Printf("State: %s\n", state)
+}
